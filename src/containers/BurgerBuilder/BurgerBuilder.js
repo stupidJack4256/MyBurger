575ffc78ae8586b965b21cf23fd09eb2fbfc3f51
Jack <anilmj4256@gmail.com>
2020-06-10 13:32:51 +0000

    added state and  installed prop typess

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/Auxilliary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+	
+	state = {
+		ingredients: {
+			salad: 1,
+			meat: 1,
+			cheese: 1,
+			bacon: 1
+		}
+	}
+	
 	render(){
 		return(
 			<Aux>
-				<div>Burger</div>
+				<Burger ingredients = {this.state.ingredients} />
 				<div>Build controls</div>
 			</Aux>
 		)
